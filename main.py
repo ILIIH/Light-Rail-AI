40cb98765cde22e24034c0cc87476a7f873abf5a
illiabranchuk <jointlord0@gmail.com>
2024-07-04 00:41:01 +0100

    code clearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
- #!/usr/bin/env python3
- # -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import numpy as np
 import tensorflow as tf
@@ -8,10 +8,9 @@ from util.prepare_data import read_images_dataset
 from util.save_model import save_model
 from util.train_model import train_model
 
-def test(X_train, Y_train):
-
+def build_model(input_shape=(1000,)):
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(1000,)),
+        tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
@@ -20,27 +19,25 @@ def test(X_train, Y_train):
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(X_train, Y_train, epochs=10, batch_size=32)
     return model
 
-def train(dataset): 
-    end_of_train_dataset = int( dataset.size - dataset.size/6 ) 
+def train(dataset):
+    end_of_train_dataset = int(dataset.size - dataset.size / 6)
 
-    train_set = dataset.iloc[:end_of_train_dataset]  
-    validation_set = dataset.iloc[end_of_train_dataset:] 
+    train_set = dataset.iloc[:end_of_train_dataset]
+    validation_set = dataset.iloc[end_of_train_dataset:]
 
-    X_train = train_set.pop('Data')
-    Y_train = train_set
+    X_train = train_set.pop('Data').values
+    Y_train = train_set.values  # Assuming 'Data' column is the input features
 
-    model = train_model(X_train,Y_train)
+    model = build_model(input_shape=(X_train.shape[1],))
+    model.fit(X_train, Y_train, epochs=10, batch_size=32, validation_data=(validation_set['Data'].values, validation_set.values))
+    
     save_model(model)
 
-
 def main():
-    image_byte_arrays =  read_images_dataset(DATASET_IMAGES_PATH, DATASET_SPECIFICATION_PATH)
+    image_byte_arrays = read_images_dataset(DATASET_IMAGES_PATH, DATASET_SPECIFICATION_PATH)
     train(image_byte_arrays)
 
 if __name__ == "__main__":
     main()
-
-
