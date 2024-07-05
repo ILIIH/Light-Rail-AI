070f15714492c08d0422b60271a0a93c0330f952
illiabranchuk <jointlord0@gmail.com>
2024-07-05 10:27:27 +0100

    code clearance

--- main.py
+++ main.py
@@ -8,18 +8,7 @@ from util.prepare_data import read_images_dataset
 from util.save_model import save_model
 from util.train_model import train_model
 
-def build_model(input_shape=(1000,)):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-    ])
 
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-
-    return model
 
 def train(dataset):
     end_of_train_dataset = int(dataset.size - dataset.size / 6)
@@ -28,11 +17,9 @@ def train(dataset):
     validation_set = dataset.iloc[end_of_train_dataset:]
 
     X_train = train_set.pop('Data').values
-    Y_train = train_set.values  # Assuming 'Data' column is the input features
+    Y_train = train_set.values  
 
-    model = build_model(input_shape=(X_train.shape[1],))
-    model.fit(X_train, Y_train, epochs=10, batch_size=32, validation_data=(validation_set['Data'].values, validation_set.values))
-    
+    model = train_model(X_train, Y_train)    
     save_model(model)
 
 def main():
--- util/const.py
+++ util/const.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+
+
 DATASET_IMAGES_PATH = './dataset/ISIC_Data_Images_Descriptions/Data/Images'
 DATASET_SPECIFICATION_PATH = './dataset/data.csv'
 
@@ -6,7 +9,7 @@ NUM_CLASSES = 3
 CONV_LAYER_PARAMS = [
     {'filters': 16, 'kernel_size': (3, 3), 'activation': 'relu'},
     {'filters': 16, 'kernel_size': (3, 3), 'activation': 'relu'},
-    {'filters': 16, 'kernel_size': (3, 3), 'activation': 'relu'}
+    {'filters': 16, 'kernel_size': (3, 3), 'activation': 'reslu'}
 ]
 DENSE_LAYER_SIZE = 256
 ACTIVATION_OUTPUT = 'sigmoid'
